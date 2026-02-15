578c45d63eeb438c1cc7d4fac56783522b42089f
Abelsaba06 <abelsabatermunoz@gmail.com>
2026-02-15 22:01:04 +0000

    Carret millores

--- camifut/public/js/carret.js
+++ camifut/public/js/carret.js
@@ -4,7 +4,6 @@ if (addToCartBtn) {
         let productId = this.getAttribute('data-id');
         let quantity = document.getElementById('quantity').value;
         let patches = document.getElementById('parches-select').value;
-
         // Obtindre la talla seleccionada
         let size = '';
         let activeSizeBtn = document.querySelector('.size-btn.active');
@@ -14,7 +13,6 @@ if (addToCartBtn) {
             alert('Per favor, selecciona una talla.');
             return;
         }
-
         // Obtindre la personalització
         let personalization = '';
         let isCustom = document.querySelector('.personalitzacio-btn').classList.contains('active');
@@ -25,14 +23,11 @@ if (addToCartBtn) {
                 personalization = `${name} - ${number}`;
             }
         }
-
         // Construir la URL amb els paràmetres de consulta
         let url = `/cart/add/${productId}?quantity=${quantity}&size=${encodeURIComponent(size)}&personalization=${encodeURIComponent(personalization)}&patches=${encodeURIComponent(patches)}`;
-
         fetch(url, {
             method: 'POST'
-        })
-            .then(response => response.json())
+        }).then(response => response.json()).then(window.location.reload())
     };
 }
 
